5f7cb3f1296ed455ec7bf5c532510e0ac486816f
Benjamin Van Ryseghem <benvr@wanadoo.fr>
2013-11-19 17:22:46 +0100

    Add echo

--- commit.sh
+++ commit.sh
@@ -1,1 +1,5 @@
+ echo =========================================================
+ echo COMMITING *.md FILES
+ echo =========================================================
+
  find . -name "*md" | xargs git add ;  find . -name "*md" | xargs git commit -m "add md output" | git push git@github.com:BenjaminVanRyseghem/Spec_Documentation.git master
