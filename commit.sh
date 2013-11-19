51f40c87b049e6e9e7c08e67ae5f072cab7827d6
Benjamin Van Ryseghem <benvr@wanadoo.fr>
2013-11-19 17:02:48 +0100

    update the script to make push succeed

--- commit.sh
+++ commit.sh
@@ -1,1 +1,1 @@
- find . -name "*md" | xargs git add ;  find . -name "*md" | xargs git commit -m "add md output" | git push
+ find . -name "*md" | xargs git add ;  find . -name "*md" | xargs git commit -m "add md output" | git push git@github.com:BenjaminVanRyseghem/Spec_Documentation.git master
