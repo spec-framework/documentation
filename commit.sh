a9f19c7fecc56779d1e0cf1104a759228656c307
Benjamin Van Ryseghem <benvr@wanadoo.fr>
2014-01-09 15:06:45 -0300

    Remove commit script

--- commit.sh
+++
@@ -1,5 +0,0 @@
- echo =========================================================
- echo COMMITING *.md FILES
- echo =========================================================
-
- find . -name "*md" | xargs git add ;  find . -name "*md" | xargs git commit -m "add md output" | git push git@github.com:BenjaminVanRyseghem/Spec_Documentation.git master
